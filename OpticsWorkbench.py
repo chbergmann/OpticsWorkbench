a991a4e0238de57256ae2629409016e7aadb366e
MSLDgit <39104085+MSLDgit@users.noreply.github.com>
2023-03-25 22:55:45 +0100

    Update OpticsWorkbench.py
    makeGrating function is implemented. Rays are changed to have orders

--- OpticsWorkbench.py
+++ OpticsWorkbench.py
@@ -4,7 +4,6 @@ import os
 from FreeCAD import Vector, Rotation, activeDocument
 import Ray
 import OpticalObject
-import SunRay
 from numpy import linspace
 from importlib import reload
 
@@ -30,7 +29,8 @@ def makeRay(position = Vector(0, 0, 0),
             hideFirst = False,
             maxRayLength = 1000000,
             maxNrReflections = 200,
-            wavelength = 580):
+            wavelength = 580,
+            order = 0):
     reload(Ray)
     '''Python command to create a light ray.'''
     name = 'Ray'
@@ -40,7 +40,7 @@ def makeRay(position = Vector(0, 0, 0),
     fp = activeDocument().addObject('Part::FeaturePython', name)
     fp.Placement.Base = position
     fp.Placement.Rotation = Rotation(Vector(1, 0, 0), direction)
-    Ray.RayWorker(fp, power, spherical, beamNrColumns, beamNrRows, beamDistance, hideFirst, maxRayLength, maxNrReflections, wavelength)
+    Ray.RayWorker(fp, power, spherical, beamNrColumns, beamNrRows, beamDistance, hideFirst, maxRayLength, maxNrReflections, wavelength, order)
     Ray.RayViewProvider(fp.ViewObject)
     recompute()
     return fp
@@ -49,30 +49,39 @@ def makeRay(position = Vector(0, 0, 0),
 def makeSunRay(position = Vector(0, 0, 0),
             direction = Vector(1, 0, 0),
             power = True,
+            beamNrColumns = 1,
+            beamNrRows = 1,
+            beamDistance = 0.1,
+            spherical = False,
             hideFirst = False,
             maxRayLength = 1000000,
-            maxNrReflections = 200,
-            wavelength_from = 400,
-            wavelength_to = 800,
-            num_rays = 100):
-    reload(SunRay)
+            maxNrReflections = 2000,
+            wavelength_from = 450,
+            wavelength_to = 750,
+            num_rays = 70,
+            order = 1):
+    reload(Ray)
+    doc = activeDocument()
     rays = []
     for l in linspace(wavelength_from, wavelength_to, num_rays):
         ray = makeRay(position = position,
             direction = direction,
             power = power,
+            beamNrColumns=beamNrColumns,
+            beamNrRows=beamNrRows,
+            beamDistance=beamDistance,
+            spherical=spherical,
             hideFirst = hideFirst,
             maxRayLength = maxRayLength,
             maxNrReflections = maxNrReflections,
-            wavelength = l)
+            wavelength = l,
+            order = order)
         ray.ViewObject.LineWidth = 1
         rays.append(ray)
 
-    fp = activeDocument().addObject('Part::FeaturePython','SunRay')
-    SunRay.SunRayWorker(fp, rays)
-    SunRay.SunRayViewProvider(fp.ViewObject)
+    group = doc.addObject('App::DocumentObjectGroup','SunRay')
+    group.Group = rays
     recompute()
-    return fp
     
 
 def restartAll():
@@ -91,6 +100,7 @@ def allOff():
     recompute()
 
 def makeMirror(base = []):
+    reload(OpticalObject)
     '''All FreeCAD objects in base will be optical mirrors.'''
     fp = activeDocument().addObject('Part::FeaturePython', 'Mirror')
     OpticalObject.OpticalObjectWorker(fp, base)
@@ -99,6 +109,7 @@ def makeMirror(base = []):
     return fp
 
 def makeAbsorber(base = []):
+    reload(OpticalObject)
     '''All FreeCAD objects in base will be optical light absorbers.'''
     fp = activeDocument().addObject('Part::FeaturePython', 'Absorber')
     OpticalObject.OpticalObjectWorker(fp, base, type = 'absorber')
@@ -107,9 +118,19 @@ def makeAbsorber(base = []):
     return fp
 
 def makeLens(base = [], RefractionIndex = 0, material = 'Quartz'):
+    reload(OpticalObject)
     '''All FreeCAD objects in base will be optical lenses.'''
     fp = activeDocument().addObject('Part::FeaturePython', 'Lens')
     OpticalObject.LensWorker(fp, base, RefractionIndex, material)
+    OpticalObject.OpticalObjectViewProvider(fp.ViewObject)
+    recompute()
+    return fp
+
+def makeGrating(base=[], RefractionIndex=1, material='', lpm = 500, GratingType = "reflection", GratingLinesPlane = Vector(0,1,0), order = 1):
+    reload(OpticalObject)
+    '''All FreeCAD objects in base will be diffraction gratings.'''
+    fp = activeDocument().addObject('Part::FeaturePython', 'Grating')
+    OpticalObject.GratingWorker(fp, base, RefractionIndex, material, lpm, GratingType, GratingLinesPlane, order)
     OpticalObject.OpticalObjectViewProvider(fp.ViewObject)
     recompute()
     return fp
@@ -120,14 +141,21 @@ def isRay(obj):
 def plot_xy(absorber):
     import numpy as np
     import matplotlib.pyplot as plt
-    
+
     coords = []
     attr_names = [attr for attr in dir(absorber) if attr.startswith('HitCoordsFrom')]
     coords_per_beam = [getattr(absorber, attr) for attr in attr_names]
     all_coords = np.array([coord for coords in coords_per_beam for coord in coords])
+    print("attr_names", attr_names)
+    print("coords_per_beam", coords_per_beam)
+    print("all_coords", all_coords)
+    
     x = -all_coords[:,1]
     y = all_coords[:,2]       
-    
+
     if len(all_coords) > 0:
         plt.scatter(x, y)
         plt.show()
+
+def isOpticalObject(obj):
+    return obj.TypeId == 'Part::FeaturePython' and hasattr(obj, 'OpticalType') and hasattr(obj, 'Base')
