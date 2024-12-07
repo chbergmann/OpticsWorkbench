d129fba8946d78edb1512272e753459a4dc3b197
hasecilu <hasecilu@tuta.io>
2024-12-07 09:15:29 -0600

    Add script to update translations

---
+++ translations/OpticsWorkbench.ts
@@ -0,0 +1,464 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>2D Radial Beam</name>
+    <message>
+        <location filename="../Ray.py" line="760"/>
+        <source>2D Radial Beam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Ray.py" line="764"/>
+        <source>Rays coming from one point going to all directions in a 2D plane</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Absorber</name>
+    <message>
+        <location filename="../OpticalObject.py" line="323"/>
+        <source>Optical Absorber</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="326"/>
+        <source>Declare your FreeCAD objects to be optical absorbers</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Beam</name>
+    <message>
+        <location filename="../Ray.py" line="735"/>
+        <source>2D Beam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Ray.py" line="738"/>
+        <source>A row of multiple rays for raytracing</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Emitter</name>
+    <message>
+        <location filename="../OpticalObject.py" line="406"/>
+        <source>Optical Emitter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="409"/>
+        <source>Declare your FreeCAD objects to be optical emitters</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Grating</name>
+    <message>
+        <location filename="../OpticalObject.py" line="119"/>
+        <source>FreeCAD objects to be diffraction gratings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="121"/>
+        <source>Refractive Index at 580nm (depends on material)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="127"/>
+        <source>Usually noted in (µm)² in literature,</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="129"/>
+        <source>lines per millimeter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="131"/>
+        <source>reflection or transmission</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="133"/>
+        <source>Normal of a hypothetical set of planes that intersect the grating surface, to define the rulings of the grating as these intersection lines</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="135"/>
+        <source>order of diffraction, set by grating</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="137"/>
+        <source>if true, order of grating overrides order of ray, if false, ray order is used</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="145"/>
+        <source>Count number and coordinates of ray hits</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="377"/>
+        <source>Diffraction grating</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="380"/>
+        <source>Declare your FreeCAD objects to be diffraction gratings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Hits2CSV</name>
+    <message>
+        <location filename="../Plot.py" line="126"/>
+        <source>Ray Hits to Spreadsheet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Plot.py" line="130"/>
+        <source>Export Ray Hits to Spreadsheet</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Lens</name>
+    <message>
+        <location filename="../OpticalObject.py" line="51"/>
+        <source>Lens</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="53"/>
+        <source>FreeCAD objects to be lenses</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="55"/>
+        <source>Refractive Index at 580nm (depends on material)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="61"/>
+        <source>Usually noted in (µm)² in literature,</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="64"/>
+        <source>Count number and coordinates of ray hits</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="350"/>
+        <source>Optical Lens</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="353"/>
+        <source>Declare your FreeCAD objects to be optical lenses</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Mirror</name>
+    <message>
+        <location filename="../OpticalObject.py" line="29"/>
+        <source>FreeCAD objects to be mirrors or absorbers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="31"/>
+        <source>Count number and coordinates of ray hits</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="295"/>
+        <source>Optical Mirror</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OpticalObject.py" line="299"/>
+        <source>Declare your FreeCAD objects to be optical mirrors</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Off</name>
+    <message>
+        <location filename="../Ray.py" line="837"/>
+        <source>Switch off lights</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Ray.py" line="842"/>
+        <source>Switch off all rays and beams</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Ray</name>
+    <message>
+        <location filename="../Ray.py" line="48"/>
+        <source>False=Beam in one direction, True=Radial or spherical rays</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Ray.py" line="50"/>
+        <source>On or Off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Ray.py" line="52"/>
+        <location filename="../Ray.py" line="54"/>
+        <source>number of rays in a beam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Ray.py" line="56"/>
+        <source>distance between two beams</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Ray.py" line="58"/>
+        <source>hide the first part of every ray</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Ray.py" line="60"/>
+        <source>maximum length of a ray</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Ray.py" line="62"/>
+        <source>maximum number of reflections</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Ray.py" line="64"/>
+        <source>Wavelength of the ray in nm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Ray.py" line="66"/>
+        <source>Order of the ray</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Ray.py" line="68"/>
+        <source>Angle of ray in case of Cone in degrees</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Ray.py" line="70"/>
+        <source>Optical Objects to ignore in raytracing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Ray.py" line="72"/>
+        <location filename="../Ray.py" line="86"/>
+        <source>FreeCAD object used as optical emitter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Ray.py" line="360"/>
+        <source>Counts the hits from</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Ray.py" line="361"/>
+        <location filename="../Ray.py" line="371"/>
+        <source>read only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Ray.py" line="370"/>
+        <source>Hit coordinates from</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Ray (monochrome)</name>
+    <message>
+        <location filename="../Ray.py" line="27"/>
+        <source>A single ray for raytracing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Ray.py" line="685"/>
+        <source>Ray (monochrome)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Ray (sun light)</name>
+    <message>
+        <location filename="../Ray.py" line="710"/>
+        <source>Ray (sun light)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Ray.py" line="713"/>
+        <source>A bunch of rays with different wavelengths of visible light</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RayHits</name>
+    <message>
+        <location filename="../Plot.py" line="100"/>
+        <source>2D/3D Plot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Plot.py" line="104"/>
+        <source>Show selected absorbers ray hits in scatter plot</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Spherical Beam</name>
+    <message>
+        <location filename="../Ray.py" line="786"/>
+        <source>Spherical Beam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Ray.py" line="790"/>
+        <source>Rays coming from one point going to all directions</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Start</name>
+    <message>
+        <location filename="../Ray.py" line="812"/>
+        <location filename="../Ray.py" line="815"/>
+        <source>(Re)start simulation</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Sun Ray</name>
+    <message>
+        <location filename="../SunRay.py" line="17"/>
+        <source>Declare your FreeCAD objects to be optical mirrors, lenses or absorbers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SunRay.py" line="22"/>
+        <source>Create rays of different wavelength</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SunRay.py" line="106"/>
+        <source>Ray (sun light)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SunRay.py" line="110"/>
+        <source>Declare your FreeCAD objects to be optical mirrors</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Workbench</name>
+    <message>
+        <location filename="../InitGui.py" line="23"/>
+        <source>Ray Tracing Simulation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="34"/>
+        <source>Ray (monochrome)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="35"/>
+        <source>Ray (sun light)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="36"/>
+        <source>Beam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="37"/>
+        <source>2D Radial Beam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="39"/>
+        <source>Spherical Beam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="39"/>
+        <source>Emitter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="40"/>
+        <source>Mirror</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="41"/>
+        <source>Grating</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="42"/>
+        <source>Absorber</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="44"/>
+        <source>Lens</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="44"/>
+        <source>Off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="45"/>
+        <source>Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="45"/>
+        <source>RayHits</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="46"/>
+        <source>Hits2CSV</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="47"/>
+        <source>Example2D</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="48"/>
+        <source>Example3D</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="49"/>
+        <source>ExampleDispersion</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="51"/>
+        <source>ExampleCandle</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
